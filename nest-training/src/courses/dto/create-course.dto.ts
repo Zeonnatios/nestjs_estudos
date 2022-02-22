b0828b69c12959658618df34cc08af269fe76e47
Matheus Antonio <matheusantonio232@gmail.com>
2022-02-22 13:44:07 -0300

    aplicando class-validator no create-course-dto

--- nest-training/src/courses/dto/create-course.dto.ts
+++ nest-training/src/courses/dto/create-course.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateCourseDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsString({ each: true })
+  @IsNotEmpty()
   readonly tags: string[];
 }
